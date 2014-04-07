844e403c1e71a0399598767366fd2a6a611e8c4f
Kevin Langley <me@kevinlangleyjr.com>
2014-04-07 17:08:14 -0400

    fixing some syntax in js

--- js/voce-post-expiration.js
+++ js/voce-post-expiration.js
@@ -1,63 +1,75 @@
-var clearExpirationValues;
 (function($){
 	$('.misc-pub-section.curtime.misc-pub-section-last').removeClass('misc-pub-section-last');
 
-	var stamp = $('#expire-datestamp').html();
-	$('.edit-expire-datestamp').click(function () {
-		if ($('#expire-datestamp-value').is(":hidden")) {
-			$('#expire-datestamp-value').slideDown("normal");
+	var clearExpirationValues,
+	stamp = $('#expire-datestamp').html();
+
+	$('.edit-expire-datestamp').click( function(e) {
+		e.preventDefault();
+		if ($('#expire-datestamp-value').is(':hidden')) {
+			$('#expire-datestamp-value').slideDown('normal');
 			$('.edit-expire-datestamp').hide();
 		}
-		return false;
-	});
+	} );
 
-	$('.cancel-expire-datestamp').click(function() {
-		$('#expire-datestamp-value').slideUp("normal");
-		$('.expire-datestamp-wrap').find('input').each(function(){
-			if(typeof $(this).data('original-value') !== 'undefined'){
-				$(this).val($(this).data('original-value'));
+	$('.cancel-expire-datestamp').click( function(e) {
+		e.preventDefault();
+
+		$('#expire-datestamp-value').slideUp('normal');
+		$('.expire-datestamp-wrap').find('input').each( function(i, e) {
+			if( typeof $(this).data('original-value') !== 'undefined' ){
+				$(this).val( $(this).data('original-value') );
 			}
-		});
+		} );
 		$('#expire-datestamp').html(stamp);
 		$('.edit-expire-datestamp').show();
 		$('.expire-datestamp-wrap').data('set', 'false');
-		return false;
-	});
+	} );
 
-	$('.remove-expire-datestamp').click(function() {
-		$('#expire-datestamp-value').slideUp("normal");
+	$('.remove-expire-datestamp').click( function(e) {
+		e.preventDefault();
+
 		clearExpirationValues();
+		$('#expire-datestamp-value').slideUp('normal');
 		$('#expire-datestamp').html('No Expiration Set');
 		$('.edit-expire-datestamp').show();
 		$('.expire-datestamp-wrap').data('set', 'true');
-		return false;
-	});
+	} );
 
-	$('#publish, #save-post').on('click', function(){
-		if(! $('.expire-datestamp-wrap').data('set')){
+	$('#publish, #save-post').on('click', function(e, i){
+		e.preventDefault();
+
+		if( !$('.expire-datestamp-wrap').data('set') )
 			clearExpirationValues();
-		}
 	});
 
-	$('.save-expire-datestamp').click(function () { // crazyhorse - multiple ok cancels
+	$('.save-expire-datestamp').click( function(e) {
+		e.preventDefault();
+
 		var year = $('#expire_year').val(),
 		month = $('#expire_month').val(),
 		day = $('#expire_day').val(),
 		hour = $('#expire_hour').val(),
 		minute = $('#expire_minute').val(),
 		attemptedDate = new Date( year, month - 1, day, hour, minute ),
-		originalDate = new Date( $('#expire_year').data('original_value'), $('#expire_month').data('original_value'), $('#expire_day').data('original_value'), $('#expire_hour').data('original_value'), $('#expire_minute').data('original_value') );
+		originalDate = new Date(
+			$('#expire_year').data('original_value'),
+			$('#expire_month').data('original_value'),
+			$('#expire_day').data('original_value'),
+			$('#expire_hour').data('original_value'),
+			$('#expire_minute').data('original_value')
+		);
 
-		if ( attemptedDate.getFullYear() !== year || (1 + attemptedDate.getMonth()) !== month || attemptedDate.getDate() !== day || attemptedDate.getMinutes() !== minute ) {
+		if ( attemptedDate.getFullYear() != year || (1 + attemptedDate.getMonth()) != month || attemptedDate.getDate() != day || attemptedDate.getMinutes() != minute ) {
 			$('.expire-datestamp-wrap', '#expire-datestamp-value').addClass('form-invalid');
 			return false;
 		} else {
 			$('.expire-datestamp-wrap', '#expire-datestamp-value').removeClass('form-invalid');
 		}
 
-		$('#expire-datestamp-value').slideUp("normal");
+		$('#expire-datestamp-value').slideUp('normal');
 		$('.edit-expiredatestamp').show();
-		if ( originalDate.toUTCString() === attemptedDate.toUTCString() ) { //hack
+		if ( originalDate.toUTCString() == attemptedDate.toUTCString() ) { //hack
 			$('#expire-datestamp').html(stamp);
 		} else {
 			$('#expire-datestamp').html(
@@ -69,10 +81,9 @@ var clearExpirationValues;
 				minute + '</b> '
 			);
 		}
-		$('#expire-datestamp-value').slideUp("normal");
+		$('#expire-datestamp-value').slideUp('normal');
 		$('#expire-datestamp-value').siblings('a.edit-expire-datestamp').show();
 		$('.expire-datestamp-wrap').data('set', 'true');
-		return false;
 	});
 
 	clearExpirationValues = function(){
@@ -82,5 +93,5 @@ var clearExpirationValues;
 		$('#expire_minute').val('');
 		$('#expire_second').val('');
 		$('#expire_month').val('');
-	};
+	}
 })(jQuery);
